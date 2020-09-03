a75e9e97079c9d7fbeef861075407d1d38162b88
Stuart Longland <stuartl@vrt.com.au>
2020-09-03 14:30:59 +1000

    WC-3138: pyhaystack init: Clean up merge artefacts

--- pyhaystack/__init__.py
+++ pyhaystack/__init__.py
@@ -16,10 +16,11 @@ try:
 
     Q_ = Quantity
     from .client.loader import get_instance as connect
-
     import requests.packages.urllib3
     from requests.packages.urllib3.exceptions import SubjectAltNameWarning
 
     requests.packages.urllib3.disable_warnings(SubjectAltNameWarning)
+
+
 except ImportError:
     pass
